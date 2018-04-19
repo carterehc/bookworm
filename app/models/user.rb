4d9332a151e68604c2f0f63fb68110106f36557b
Emily Carter <cartere@berea.edu>
2018-04-19 18:02:42 +0000

    before save downcase username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save {self.username = username.downcase}
     validates :username, presence: true, length: {minimum: 1, maximum: 100}, uniqueness: true
     validates :password, presence: true, length: {minimum: 6, maximum: 100}
     
