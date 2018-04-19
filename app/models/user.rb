47d9bfd20381d25442a9308d2f384c5e8175b67a
Emily Carter <cartere@berea.edu>
2018-04-19 17:50:43 +0000

    validate unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: {minimum: 1, maximum: 100}
+    validates :username, presence: true, length: {minimum: 1, maximum: 100}, uniqueness: true
     validates :password, presence: true, length: {minimum: 6, maximum: 100}
     
 end
